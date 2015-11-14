baa523639d870c23fbfd2c59f43d910292266d72
Art Beatte <art.beatte@docusign.com>
2015-11-14 15:29:05 -0800

    Creates TestAll entry point to run all exercise tests.

---
+++ com/artbeatte/exercises/TestAll.java
@@ -0,0 +1,27 @@
+package com.artbeatte.exercises;
+
+import com.artbeatte.exercises.bst.Bst;
+import com.artbeatte.exercises.strings.RunLengthEncoding;
+import com.artbeatte.exercises.sudoku.Sudoku;
+
+/**
+ * @author art.beatte
+ * @version 11/14/15
+ */
+public class TestAll {
+
+    /**
+     * Kicks off test suites for all enrolled projects
+     * @param args system args
+     */
+    public static void main(String[] args) {
+        Bst.main(args);
+        RunLengthEncoding.main(args);
+        Sudoku.main(args);
+
+//        HashMapWTime.main(args);
+//        MeetingScheduler.main(args);
+
+//        ElevatorBankTest.main(args);
+    }
+}
